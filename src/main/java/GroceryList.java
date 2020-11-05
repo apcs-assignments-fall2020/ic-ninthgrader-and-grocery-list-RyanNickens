0487f4241c80d18c7f92eff483dbbcbab3346823
RyanNickens <22rnickens@athenian.org>
2020-11-05 02:08:06 +0000

    Grocery List: passes tests, iterates through array.

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,3 +1,44 @@
+//Ryan Nickens
+import java.util.stream.*;
+import java.util.Arrays;
+
 public class GroceryList {
+    private String[] arr;
+
+    public GroceryList() {
+        // our grocery list will have a max size of 10
+        this.arr = new String[10]; 
+    }
+
+    //adding item to the grocery list
+    public void add(String item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                arr[i] = item;
+                break;
+            }
+        }
+    }
+   
+    public void remove(String item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == item) {
+                arr[i] = null;
+                break;
+            }
+        }
+
+    }
+    public String toString() {
+        String x = Arrays.toString(arr);
+        //System.out.println(Arrays.toString(arr));
+        //System.out.println(x);
+        return x;
+    }
+    
+
+
+    //if (arr[i] == null); checks to see if value in array is null
+    //if (arr[i] == null); next line arr[i+1] = arr[i]
     
 }
--- src/main/java/NinthGrader.java
+++ src/main/java/NinthGrader.java
@@ -1,3 +1,26 @@
-public class NinthGrader {
+public class NinthGrader extends Student {
+
+    //private int classYear;
+
+    public NinthGrader() {
+        super("jim", "john", 2024);
+        //java will automatically call the super with no parameters from parent class
+    }
+
+     public NinthGrader(String a, String b) {
+        super(a, b, 2024);
+    }
+    
+    @Override
+    public void doSomething() {
+        System.out.println("I'm preparing for a harkness discussion");
+    }
+
+    @Override
+    public String toString() {
+        String str = this.getFirstName() + " " + this.getLastName() + ", " + this.getClassYear() + " (ninth grader)";
+        return str;
+    }
+
     
 }
